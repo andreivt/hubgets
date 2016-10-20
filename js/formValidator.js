ee227c4e9fe6839dcd75a031ab3dfbce8ae73efe
nikodemus0715 <andrei.tertiscu@gmail.com>
2016-10-20 16:43:00 +0300

    Modify FormValidator for all input fiels

--- js/formValidator.js
+++ js/formValidator.js
@@ -1,39 +1,111 @@
 'use strict';
 
-var submitButton = document.getElementById('submit-form');
+var form = document.getElementsByClassName('form-horizontal')[0];
+var formFields = {
+    fname   : document.getElementById("fname"),
+    lname   : document.getElementById("lname"),
+    company : document.getElementById("company"),
+    email   : document.getElementById("email"),
+};
 
-submitButton.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    var emailField = document.getElementById('email');
-    var parentNode = emailField.parentNode;
-    var iconElement = emailField.parentNode.querySelector('span');
-
-    if (validateEmail(emailField.value)) {
-        removeClassFromElement(' has-error', parentNode);
-        removeClassFromElement(' has-feedback', parentNode);
-        addClassToElement(' sr-only', iconElement);
-        emailField.style.backgroundColor = 'initial';
-        document.getElementById('emailInputStatus').className = 'sr-only';
-    } else {
-        addClassToElement(' has-error', parentNode);
-        addClassToElement(' has-feedback', parentNode);
-        removeClassFromElement(' sr-only', iconElement);
-        emailField.style.backgroundColor = '#fde7e9';
-        document.getElementById('emailInputStatus').className = 'input-error';
-    }
+form.addEventListener('change', function(e){
+    validateFields();
 })
-
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    validateFields();
+})
+/**
+ * Checks which fields are(not) empty
+ * @param  {object} object Contains form inputs refs
+ * @return {object}        Contains IDs of empty/filled fields
+ */
+function checkEmptyFields(object) {
+    var empty = [],
+        filled = [];
+    for (var k in object) {
+        if (object.hasOwnProperty(k)) {
+            if(object[k].value) {
+              if(object[k].id == 'email'){
+                validateEmail(object[k].value) ? filled.push(object[k].id) : empty.push(object[k].id)
+              }else{
+                filled.push(object[k].id)
+              }
+            }else{
+              empty.push(object[k].id)
+            }
+        }
+    }
+    return {
+        empty: empty,
+        filled: filled
+    };
+}
+/**
+ * Applies coresponding effect on filled/empty input fields
+ */
+function validateFields() {
+    var response = checkEmptyFields(formFields);
+    var emptyFields = response.empty;
+    var filledFields = response.filled;
+    unsetFields(filledFields);
+    setFields(emptyFields);
+}
+/**
+ * Removes error effect from given input fields
+ * @param {array} filledFields Given fields
+ */
+function unsetFields(filledFields) {
+  if (filledFields.length) {
+      for (let i = 0; i < filledFields.length; i++) {
+          removeClassOfElement(' has-error', formFields[filledFields[i]].parentNode);
+          removeClassOfElement(' has-feedback', formFields[filledFields[i]].parentNode);
+          var iconElement = formFields[filledFields[i]].parentNode.querySelector('span');
+          addClassToElement(' sr-only', iconElement);
+          formFields[filledFields[i]].style.backgroundColor = 'initial';
+          formFields[filledFields[i]].parentNode.lastElementChild.className = 'sr-only';
+      }
+  }
+}
+/**
+ * Sets error effect on given fields
+ * @param {arraay} emptyFields Given fields
+ */
+function setFields(emptyFields) {
+  if (emptyFields.length) {
+      for (let i = 0; i < emptyFields.length; i++) {
+          addClassToElement(' has-error', formFields[emptyFields[i]].parentNode);
+          addClassToElement(' has-feedback', formFields[emptyFields[i]].parentNode);
+          var iconElement = formFields[emptyFields[i]].parentNode.querySelector('span');
+          removeClassOfElement(' sr-only', iconElement);
+          formFields[emptyFields[i]].style.backgroundColor = '#fde7e9';
+          formFields[emptyFields[i]].parentNode.lastElementChild.className = 'input-error';
+      }
+  }
+}
+/**
+ * Checks if given string is a valid email
+ * @param  {string} email Given email
+ * @return {boolean}      True if email is valid, otherwise false
+ */
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
-
-function removeClassFromElement(className, element) {
+/**
+ * Removes given className of an element
+ * @param  {string} className className to be removed
+ * @param  {element} element  Element from which the className will be removed
+ */
+function removeClassOfElement(className, element) {
     element.className = element.className.replace(className, '');
 }
-
+/**
+ * Adds given className to an element
+ * @param {string} className className to be added
+ * @param {element} element   Element
+ */
 function addClassToElement(className, element) {
-    removeClassFromElement(className, element);
+    removeClassOfElement(className, element);
     element.className += className;
 }
